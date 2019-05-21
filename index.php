353ec2eb5b9425dd50226cb1b9065cfab04ff680
bukharituharea <48221953+bukharituharea@users.noreply.github.com>
2019-05-21 23:21:00 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<html><body>
+
+<?php
+	$nama = $NIM ="";
+	if($_SERVER["REQUEST_METHOD"]== "POST"&& isset($_POST["nama"])&& isset($_POST["NIM"])){
+		$nama =$_POST["nama"];
+		$NIM = $_POST["NIM"];
+	}
+?>
+
+<form method="POST"action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+	<p>Bukhari F.A. Tuharea</p>
+	Nama 	:<input type="text" name="nama" min="1"><br>
+	Nim 	:<input type="number" name="NIM"><br>
+	<input type="submit">
+</form>
+
+<?php
+	if(empty($nama) && empty($NIM) ){
+	echo "Nama dan Nim Tidak Boleh Kosong!!";
+	}
+
+	elseif (!empty($nama)&& (($NIM)>9999999999)){
+	echo "NIM lebih dari 10 angka";
+	}
+
+	elseif (!empty($nama)&& !empty($NIM)){
+	echo "<h2> Selamat Datang</h2>";
+	echo $nama ."<br>".$NIM;
+	}
+
+?>
+</body></html>
